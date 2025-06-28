010bc6824e686338d5850ccfbf7e58665b25b218
h-faber <hfaberlu@gmail.com>
2025-06-28 23:18:39 +0900

    fix: 크롬 익스텐션에서 404 오류 방지를 위해 `createHashRouter` 적용

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 
 import App from "@/App";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     Component: App,
