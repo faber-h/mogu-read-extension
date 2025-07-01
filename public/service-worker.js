45f646896037f91798509c0d8b69313f291acca9
h-faber <hfaberlu@gmail.com>
2025-07-01 16:44:05 +0900

    feat: 탭이 활성화될 때마다 Content Script를 강제로 실행하도록 추가 #7
    - activeTab, scripting 권한 설정
    - 탭 활성화 시 content.js 실행

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,8 @@
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.scripting.executeScript({
+    target: { tabId: activeInfo.tabId },
+    files: ["content.js"],
+  });
+});
