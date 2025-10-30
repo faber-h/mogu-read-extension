b398fc6be6891e03f3e6a0ab1ea681118ba5812f
h-faber <hfaberlu@gmail.com>
2025-10-30 16:15:55 +0900

    chore: ESLint TS 지원 #30

--- eslint.config.js
+++ eslint.config.js
@@ -57,5 +57,64 @@ export default defineConfig([
       ],
     },
   },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.browser,
+        ...globals.webextensions,
+      },
+      parser: await import("@typescript-eslint/parser").then((m) => m.default),
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+        project: false,
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      import: importPlugin,
+      // Typescript ruleset
+      "@typescript-eslint": await import(
+        "@typescript-eslint/eslint-plugin"
+      ).then((m) => m.default),
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...reactRefresh.configs.vite.rules,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^[A-Z_]" },
+      ],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+            },
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
   prettier,
 ]);
